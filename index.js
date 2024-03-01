342d7fd441ff009a152adf11fa02d37811fa1260
BuildTools <unconfigured@null.spigotmc.org>
2024-02-29 22:19:17 -0500

    added avatars to online players

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // https://mcapi.us/
 // https://cindr.org/how-to-get-a-minecraft-servers-player-count-motd-and-status-using-javascript/
+
+// mineskin.eu
 function initServerData(serverIp,serverPort){
     const serverIpElement = document.getElementById('server-ip');
     serverIpElement.innerHTML = serverIp;
@@ -29,10 +31,30 @@ function initServerData(serverIp,serverPort){
         for (var key in data) {
             if (key == "players" && data.players.now > 0 ){ // if the key is players and there are more than 0 players online
                 for (i = data.players.now; i > 0; i--){ // loop through players
+
+                    // create a new list element
                     const node = document.createElement("li");
-                    const textnode = document.createTextNode(data.players.sample[i - 1].name);
+
+                    // create a text node of the player's name
+                    var textnode = document.createTextNode(data.players.sample[i - 1].name);
+
+                    // add the text to the list item
                     node.appendChild(textnode);
+
+                    // get the player's avatar
+                    var img = new Image();
+                    img.src = "https://mineskin.eu/avatar/" + data.players.sample[i - 1].name;
+                    img.alt = data.players.sample[i - 1].name + "'s Avatar";
+
+                    // add the avatar pic to an avatar class
+                    img.classList.add("avatar");
+
+                    // add the player's avatar to the list item
+                    node.appendChild(img);
+
+                    // add the list item to the player list
                     document.getElementById("player-list").appendChild(node);
+
                 }
             }
             console.log(key);
